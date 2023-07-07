62e5a99323007ae32fcb2e5b2e2db50db4211b64
Andreev Dmitriy <andreev.dp@bk.ru>
2023-07-08 01:20:36 +0400

    0.2.1
    auto restart if game was over

--- Tic-Tac-Toe/src/Main.java
+++ Tic-Tac-Toe/src/Main.java
@@ -3,44 +3,47 @@ import java.util.Scanner;
 public class Main {
     static boolean flag = false;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        CoreLogic cl = new CoreLogic();
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            CoreLogic cl = new CoreLogic();
 
 
-        System.out.println("\nДобро пожаловать, данная игра является первой, которую я написал на Java!\n" +
-                "Надеюсь вам она понравится. Для навигации вводите цифры в консоль.\n\nМеню:\n" +
-                "1. Начать играть\n" +
-                "3. Как играть\n" +
-                "4. Об авторе\n" +
-                "5. Выход\n");
-        System.out.print("Player> ");
-        int menuChoise = scanner.nextInt();
-        switch (menuChoise){
-            case 1:
-                System.out.println("\n\n\nИгра началась!\n");
-                String[][] playground = cl.GetFiled();
+            System.out.println("\nДобро пожаловать, данная игра является первой, которую я написал на Java!\n" +
+                    "Надеюсь вам она понравится. Для навигации вводите цифры в консоль.\n\nМеню:\n" +
+                    "1. Начать играть\n" +
+                    "3. Как играть\n" +
+                    "4. Об авторе\n" +
+                    "5. Выход\n");
+            System.out.print("Player> ");
+            int menuChoise = scanner.nextInt();
+            switch (menuChoise){
+                case 1:
+                    System.out.println("\n\n\nИгра началась!\n");
+                    String[][] playground = cl.GetFiled();
 
-                while(!flag){
-                    cl.UniversalMethodPlayer(flag);
-                    cl.FinishSecurity(cl.GetFiled(), flag);
-                }
+                    while(!flag){
+                        cl.UniversalMethodPlayer(flag);
+                        cl.FinishSecurity(cl.GetFiled(), flag);
+                    }
 
-                cl.ShowPlayground(playground);
-                System.out.println("Данная партия закончилась ничьей");
+                    cl.ShowPlayground(playground);
+                    System.out.println("Данная партия закончилась ничьей");
 
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Некорректный пункт");
-                break;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Некорректный пункт");
+                    break;
+            }
+
         }
 
 
